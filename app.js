21c93cf0afb915dc1e6675a51a0f6b10bcc06ef6
Olivier Raulin <olivier@olivier-raulin.fr>
2015-07-19 13:15:27 +0200

    Create the first route

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var config 	= require ('config.js');
-var events 	= require ('events.js');
-//var routes 	= require ('routes');
-var app 	= require ('express')
+var config 	= require ('./config.js');
+var events 	= require ('./events.js');
+var mainRoutes 	= require ('./routes/main.js');
+var express 	= require ('express');
+
+var app = express() ;
+
+app.get( '/', mainRoutes.index );
+
+app.listen( config.http.port || 8000, function(err){
+	if( err ){ throw err; exit(1) ; }
+	console.log("Listening on port", config.http.port || 8000);
+});
